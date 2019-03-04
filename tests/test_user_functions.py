3cf955c873b3700546f4e4a4fa51bf997f5a2907
ZTB95 <ztb995@gmail.com>
2019-03-04 12:05:17 -0600

    More test-prep changes

--- tests/test_user_functions.py
+++ tests/test_user_functions.py
@@ -1,5 +1,6 @@
 import unittest
 import PythonRegistryDiffer.user_functions as exec
+import tests.testing_prep as tp
 
 
 class TestUserFunctions(unittest.TestCase):
@@ -8,15 +9,19 @@ class TestUserFunctions(unittest.TestCase):
         self.assertEqual(exec.validate_arguments(), True)
 
     def test_new_database(self):
+        tp.reset_all_changes()
         self.assertEqual(exec.new_database(), True)
 
     def test_new_image(self):
+        tp.before_diff()
         self.assertEqual(exec.new_image(), True)
 
     def test_list_images(self):
         self.assertEqual(exec.list_images(), True)
 
     def test_diff_images(self):
+        tp.before_second_diff()
+        exec.new_image()
         self.assertEqual(exec.diff_images(), True)
 
     def test_load_db(self):
