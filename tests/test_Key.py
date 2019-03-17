3c7a925be91a8e8f16feabad5b12562bfbd7355d
ZTB95 <ztb995@gmail.com>
2019-03-17 12:24:26 -0500

    Update test_Key.py

--- tests/test_Key.py
+++ tests/test_Key.py
@@ -1,17 +1,19 @@
 import unittest
-import datetime as dt
 from PythonRegistryDiffer.Key import Key as k
 
 
 class TestKey(unittest.TestCase):
-    def testkey(self):
-        date = dt.datetime.now()
+    def testkey_noval(self):
+        # Test without values
         key = k(key_path='HKEY_CLASSES_ROOT\\Test\\', modified=12345678, name='Test', dbid=1)
         self.assertEqual(key.key_path, 'HKEY_CLASSES_ROOT\\Test\\')
         self.assertEqual(key.modified, 12345678)
         self.assertEqual(key.name, 'Test')
         self.assertEqual(key.dbid, 1)
         self.assertEqual(key.values, [])
+
+    def testkey_withval(self):
+        # Test with values
         key = k(key_path='HKEY_CLASSES_ROOT\\Test\\', modified=12345678, name='Test', dbid=1, values=[1, 2, 3])
         self.assertEqual(key.values[1], 2)
         self.assertEqual(len(key.values), 3)
