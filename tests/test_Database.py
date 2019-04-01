5aa8af20e2435da1040b6b2086712947d67a1c3f
ZTB95 <ztb995@gmail.com>
2019-03-31 20:06:53 -0500

    Update test_Database.py

--- tests/test_Database.py
+++ tests/test_Database.py
@@ -18,7 +18,8 @@ class TestCreateDatabase(unittest.TestCase):
         self.assertEqual(db.hkcc, True)
 
     def test_create_database_file_true_one_key(self):
-        db = Database(location='newdatabase.db', auto_commit=True, hklm=False, hkcr=False, hkcu=False, hku=False)
+        db = Database(location='newdatabase.db', auto_commit=True, hklm=False, hkcr=False, hkcu=False, hku=False,
+                      hkcc=True)
         self.assertEqual(db.auto_commit, True)
         self.assertEqual(db.hklm, False)
         self.assertEqual(db.hkcr, False)
@@ -32,10 +33,34 @@ class TestCreateDatabase(unittest.TestCase):
 
 
 class TestDatabase(unittest.TestCase):
+
     @classmethod
     def setUp(cls):
+        cls._dbm = Database(location=':memory:', auto_commit=True)
+        cls._dbf = Database(location='testfile.db', auto_commit=True)
+
+    def test_open_database(self):
+        self._dbm.open()
+        self._dbf.open()
+        
+    def test_database_with_context_manager(self):
+        m = Machine(ip='127.0.0.1', hostname='localhost')
+        with self._dbm as db:
+            db.add_machine(machine=m)
+            # Not actually testing the add_machine here. Just making sure the context manager works.
+
+        with self._dbf as db:
+            db.add_machine(machine=m)
+            # Not actually testing the add_machine here. Just making sure the context manager works.
+
+    def test_database_insert_functions(self):
+        pass
+
+    def test_database_select_functions(self):
         pass
 
     @classmethod
     def tearDown(cls):
-        pass
+        cls._dbm.close()
+        cls._dbf.close()
+        # just in case? Should ever actually need this. close() should be called if it errors out because __exit__ calls it
