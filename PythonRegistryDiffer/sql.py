e258140fc80a471f485188b68ebbb287be57bdce
ZTB95 <ztb995@gmail.com>
2019-04-01 19:04:48 -0500

    Update sql.py
    Changes for issues discovered during Database.py testing.

--- PythonRegistryDiffer/sql.py
+++ PythonRegistryDiffer/sql.py
@@ -1,47 +1,52 @@
 # CREATES
 create_machine_table = ('CREATE TABLE Machine ( '
-                         'id INTEGER PRIMARY KEY, '
-                         'lastKnownIP VARCHAR(46), '
-                         'hostName VARCHAR(256) '
-                         ');\n')
+                        'id INTEGER PRIMARY KEY, '
+                        'lastKnownIP VARCHAR(46), '
+                        'hostName VARCHAR(256) '
+                        ');\n')
 
 create_image_table = ('CREATE TABLE RegImage ( '  # TODO Update the tables name in the DBD
-                       'id INTEGER PRIMARY KEY, '
-                       'FOREIGN KEY(machineId) REFERENCES Machine(id),'
-                       'label VARCHAR(256) NULL, '
-                       'timeTaken DATETIME NOT NULL '
-                       ');\n')
+                      'id INTEGER PRIMARY KEY, '
+                      'machineId INTEGER, '
+                      'label VARCHAR(256) NULL, '
+                      'timeTaken DATETIME NOT NULL, '
+                      'FOREIGN KEY(machineId) REFERENCES Machine(id) '
+                      ');\n')
 
 create_key_table = ('CREATE TABLE RegKey ( '
-                     'id INTEGER PRIMARY KEY, '
-                     'FOREIGN KEY(regImageId) REFERENCES RegImage(id) '
-                     'path VARCHAR(256) NOT NULL , '
-                     'lastModified DATETIME NOT NULL '
-                     ');\n')  # TODO: Remove the name column from the DBD and update the table's name
+                    'id INTEGER PRIMARY KEY, '
+                    'regImageId INTEGER, '
+                    'path VARCHAR(256) NOT NULL , '
+                    'lastModified DATETIME NOT NULL, '
+                    'FOREIGN KEY(regImageId) REFERENCES RegImage(id) '
+                    ');\n')  # TODO: Remove the name column from the DBD and update the table's name
 
 create_key_value_table = ('CREATE TABLE RegKeyValue ( '  # TODO: Update the table's name in the DBD
-                           'id INTEGER PRIMARY KEY, '
-                           'FOREIGN KEY (regKeyId) REFERENCES RegKey(id), ' # TODO: update the column name in the DBD
-                           'name VARCHAR(256) NOT NULL, '
-                           'type INTEGER NOT NULL, '
-                           'data BLOB(1048576) NOT NULL '  # 1 MB should be enough for anything in a REG_BINARY key, right? TODO: try to break this
-                           ');\n')
+                          'id INTEGER PRIMARY KEY, '
+                          'regKeyId INTEGER, ' # TODO: update the column name in the DBD
+                          'name VARCHAR(256) NOT NULL, '
+                          'type INTEGER NOT NULL, '
+                          'data BLOB(1048576) NOT NULL, '  # 1 MB should be enough for anything in a REG_BINARY key, right? TODO: try to break this
+                          'FOREIGN KEY (regKeyId) REFERENCES RegKey(id) '
+                          ');\n')
 
 create_hkeys_table = ('CREATE TABLE HKEYs ( '
-                       'id INTEGER PRIMARY KEY, '
-                       'hklm BOOLEAN NOT NULL, '
-                       'hkcu BOOLEAN NOT NULL, '
-                       'hku BOOLEAN NOT NULL, '
-                       'hkcr BOOLEAN NOT NULL, '
-                       'hkcc BOOLEAN NOT NULL '
-                       ');\n')
+                      'id INTEGER PRIMARY KEY, '
+                      'hklm BOOLEAN NOT NULL, '
+                      'hkcu BOOLEAN NOT NULL, '
+                      'hku BOOLEAN NOT NULL, '
+                      'hkcr BOOLEAN NOT NULL, '
+                      'hkcc BOOLEAN NOT NULL '
+                      ');\n')
 
-create_only_one_hkey_trigger = ("CREATE TRIGGER trg_onlyOneHKEY BEFORE INSERT "
-                                 "ON HKEYs "
-                                 "WHEN (SELECT COUNT(*) FROM HKEYs) >= 1 "
-                                 "BEGIN "
-                                 "SELECT RAISE(FAIL, 'There can only be one row in HKEYs.') "
-                                 "END;\n")
+create_only_one_hkey_trigger = ("CREATE TRIGGER trg_onlyOneHKEY "
+                                "BEFORE INSERT "
+                                "ON HKEYs "
+                                "WHEN (SELECT COUNT(*) FROM HKEYs) >= 1 "
+                                "BEGIN "
+                                "SELECT RAISE(FAIL, 'There can only be one row in HKEYs.') "
+                                "END; "
+                                "END;")
 
 enforce_foreign_keys = 'PRAGMA foreign_keys = 1'
 
